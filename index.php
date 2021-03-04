792bf699805300594a4e6b1176bd2576f9889f6e
Anna Varzina <varzinaanna@gmail.com>
2021-03-04 14:10:06 +0100

    Add welcome page

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Kempest</title>
+ </head>
+ <body style="background: url(images/bg.jpg) top center no-repeat; font-family: Arial; color:#fff;">
+  <div style="width:900px; margin:0 auto;">
+   <h1 style="text-align:center; font-size:46px; margin-top:160px;">Hi, there!</h1>
+   <a href='index.html'; style='color:#fff;'>
+	<p style="text-align:center; font-size:20px; margin-top:50px; padding-bottom:40px;">Welcome to my blog! &#128521</p>
+   </a>
+ </body>
+</html>
